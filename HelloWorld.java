704e9a3fe416f2fa7728ce6123e1775c0fc2bd26
mvpm-prog <149729776+mvpm-prog@users.noreply.github.com>
2023-11-08 01:12:30 +0330

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Ashena
+ */
+
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String args[]) {
+        ُ
+        // TODO code application logic here
+        System.out.println("helloworld");
+    }
+}
